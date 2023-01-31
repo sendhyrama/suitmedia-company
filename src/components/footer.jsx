fe4dd1625116d6a416ed8fdd8e01923a61535667
sendhyrama <sendhyrama@gmail.com>
2023-01-31 11:02:37 +0700

    update footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-neutral-800 relative flex flex-col text-white items-center py-5">
-      <span>Copyright &copy;2016.PT Company</span>
+      <span>Copyright &copy; 2016. PT Company</span>
       <div className="flex gap-2 mt-2">
         <a href="#">
           <i className="bx text-3xl bxl-facebook-square"></i>
